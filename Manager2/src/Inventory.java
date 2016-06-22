24524b14fa2824e5495b3074d4a18d921290c33c
dkhenry32 <dkhenry32@gmail.com>
2016-06-22 17:46:58 -0500

    java console app

--- Manager2/src/Inventory.java
+++ Manager2/src/Inventory.java
@@ -14,35 +14,43 @@ public class Inventory {
 		System.out.println("Name Your List");
 		list_name = user_input.next( );
         list_of_items = GetList();
-        String show = null;
-          for ( int i = 0; i < list_of_items.size() - 1; i++){
+        String show = "";
+          for ( int i = 0; i < list_of_items.size(); i++){
         	  show += list_of_items.get(i);  
         	  // not last item in list insert comma
-        	  if (i < list_of_items.size() - 1){
-        		 show += ",:";  
-        		  
+        	  if (i < list_of_items.size()){
+        		 show += ",:";   		  
         	  }
-          }
-            
-        		
+          }        		
 
 		System.out.println(list_name +": " + show );
+		send(); 
 	}
+	
    private static List<String> GetList(){
-	   System.out.println("Enter Item for your inventory Press x when finished");
+	   System.out.println("Enter Item for your inventory Press exit when finished");
         Scanner list_input = new Scanner(System.in);    	   
 	    String temp_input = list_input.next().toString();
 	    
 	    List<String> list_of_items =new ArrayList <String>(Arrays.asList());
 	    
-	    while(temp_input != "x"){
+	    while(!temp_input.equals("exit")){
 	        list_input = new Scanner(System.in);
+	        
 	    	list_of_items.add(temp_input);
-	        System.out.println("Enter Item for your inventory Press x when finished (temp_input = "+temp_input+")");
-	        temp_input = list_input.next().toString();
-	       
-	    }
-			   
+	        System.out.println("Enter Item for your inventory Press exit when finished ('temp_input')");
+	        temp_input = list_input.next();       
+	    }   
+	    
 	   return list_of_items;   
+}
+   private static void send(){
+	   System.out.println("Do you want to email Inventory enter yes or no");
+	    Scanner email = new Scanner(System.in);
+	      if (email.next().equals("yes")){
+	    		  System.out.println("Here is your email");
+   } else  {
+	         System.out.println("Have a nice day");
    }
+  }  	     
 }
