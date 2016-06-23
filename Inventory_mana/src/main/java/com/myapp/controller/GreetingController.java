17d84f83d9752476cac84fe6e2ecdb88e8bd1f98
dkhenry32 <dkhenry32@gmail.com>
2016-06-23 00:40:48 -0500

    bare app

--- Inventory_mana/src/main/java/com/myapp/controller/GreetingController.java
+++ Inventory_mana/src/main/java/com/myapp/controller/GreetingController.java
@@ -1,10 +1,39 @@
 package com.myapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value="/greeting", method=RequestMethod.GET)
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @RequestMapping(value="/greeting", method=RequestMethod.POST)
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+
+}
+/*package com.myapp.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.myapp.dao.Member;
 
@@ -15,12 +44,28 @@ public class GreetingController {
 	JdbcTemplate jdbcTemplate;
 	
 	
-	@RequestMapping("/greeting")
+	@RequestMapping(value="/greeting", method=RequestMethod.GET)
 	public String greeting(Model model){
 		
-	Member me =	jdbcTemplate.queryForObject("select * FROM member", Member.class);
-		
-		model.addAttribute("name",me.getName());
+	//Member me =	jdbcTemplate.queryForObject("select * FROM member", Member.class);
+		//model = new Greeting();
+		Greeting greet = new Greeting();
+		greet.firstName = "Darrell";
+		model.addAttribute("greeting", greet);
+
 		return "greeting";
 	}
+	
+	@RequestMapping(value="/greeting", method=RequestMethod.POST)
+	public String saveLastName(@ModelAttribute Greeting greeting, Model model){
+	//     model.addAttribute("finished", "Thanks For updating your last name");
+	     
+	     greeting.firstName = "NotDarrell";
+		greeting.finished = greeting.lastName;
+		model.addAttribute("greeting", greeting);
+	     return "greeting";
+	}
+	
 }
+
+*/
--- Inventory_mana/src/main/java/com/myapp/repository/ItemRepository.java
+++ Inventory_mana/src/main/java/com/myapp/repository/ItemRepository.java
@@ -1,8 +1,9 @@
 package com.myapp.repository;
 
+import com.myapp.domain.Item;
 import org.springframework.data.repository.CrudRepository;
 
-import com.myapp.domain.Item;
+import java.util.List;
 
 public interface ItemRepository extends CrudRepository<Item, Integer> {
 	List<Item> findByName(String name);
